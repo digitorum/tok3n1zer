6cc728b6688ffda7a7a40c989f45c03ff3694c3b
Pavel Ladygin <me@digitorum.ru>
2017-12-15 12:38:30 +0300

    Набор простых тестов для проверки парсинта HTML

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 var Parser = require("./lib/parser.js");
 var ParserStates = require("./lib/parserStates.js");
 var parser = new Parser();
-var buffers = parser.parseString('<p>An? Ancient<span>!</span> talisman. <em>An ancient talisman? <b style="display: block; border: none;">depicting</b> a <u>"</u>holy symbol "bestowed" upon the #Warriors of Sunlight...</em></p>');
+var result = parser.parseString('<p>An? Ancient<span>!</span> talisman. <em>An ancient talisman? <b style="display: block; border: none;">depicting</b> a <u>"</u>holy symbol "bestowed" upon the #Warriors of Sunlight...</em></p>');
 
-console.log("Source:", "\n");
-console.log(buffers.map(function (v) {
-    return v.value;
-}).join(""));
-console.log("\n", "Tokenized:", "\n");
-console.log(buffers.map(function (v) {
-    switch (v.state) {
-        case ParserStates.STATE_TAG_FULL:
-            return '';
-            break;
-        case ParserStates.STATE_TEXT_QUOTE_OPEN:
-            return ' {OPEN_QUOTE} ';
-            break;
-        case ParserStates.STATE_TEXT_QUOTE_CLOSE:
-            return ' {CLOSE_QUOTE} ';
-            break;
-        case ParserStates.STATE_TEXT_SENTENCE_END:
-            return ' {SENTENCE_POSSIBLE_END} ';
-            break;
-        case ParserStates.STATE_TEXT_NOT_LETTER:
-            return ' {CHAR} ';
-            break;
-        default:
-            return v.value;
-    }
-}).join("").replace(/\s+/g, ' '));
+console.log("Source:\n");
+console.log(result.getSourceString());
+console.log("\nTokenized:\n");
+console.log(result.getPseudoCode());
---
+++ tests/parser.js
@@ -0,0 +1,35 @@
+﻿var assert = require('assert');
+var parser = new (require('../lib/parser.js'));
+
+/**
+ * Сделать набор тест кейсов для проверки результата парсинга
+ *
+ * @param {any} description
+ * @param {any} source
+ * @param {any} pseudocode
+ */
+function makeTestCases(description, source, pseudocode) {
+    describe(description, function () {
+        var result = parser.parseString(source);
+        
+        it('Produse serialization equal to source', function () {
+            assert.equal(result.getSourceString(), source);
+        });
+        it('Produse pseudocode', function () {
+            assert.equal(result.getPseudoCode(), pseudocode);
+        });
+    });
+    
+}
+
+// Простые проверки на парсинг тэгов
+describe('Parser', function () {
+    makeTestCases("XHTML tag", "<BR/>", "{TAG}");
+    makeTestCases("XHTML tag with attributes {single quote, double quote, without quote, without value}", "<BR class='first-class second-class' class-dq=\"first-class second-class\" data-attr=1 readonly/>", "{TAG}");
+    makeTestCases("HTML tag (single)", "<BR>", "{TAG}");
+    makeTestCases("HTML tag (single) with attributes {single quote, double quote, without quote, without value}", "<BR class='first class second-class' class-dq=\"first class second-class\" data-attr=1 readonly>", "{TAG}");
+    makeTestCases("HTML tag (empty)", "<p></p>", "{TAG} {TAG}");
+    makeTestCases("HTML tag (empty) with attributes {single quote, double quote, without quote, without value}", "<p class='first class second-class' class-dq=\"first class second-class\" data-attr=1 readonly></p>", "{TAG} {TAG}");
+    makeTestCases("HTML tag (with inner text)", "<p>Hello world!</p>", "{TAG} Hello world {SENTENCE_POSSIBLE_END} {TAG}");
+    makeTestCases("HTML tag (with inner text) with attributes {single quote, double quote, without quote, without value}", "<p class='first class second-class' class-dq=\"first class second-class\" data-attr=1 readonly>Hello world!</p>", "{TAG} Hello world {SENTENCE_POSSIBLE_END} {TAG}");
+});
