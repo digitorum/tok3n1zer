929f4ac293f886e76e74952e2ac73f03e4e3f148
Pavel Ladygin <me@digitorum.ru>
2017-12-15 14:35:15 +0300

    Более сложный пример входного текста.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ﻿var Parser = require("./lib/parser.js");
 var ParserStates = require("./lib/parserStates.js");
 var parser = new Parser();
-var result = parser.parseString('Компания "ООО "Рога и копыта" производит рога и копыта... Цитата: "Покупайте наших слонов! Наши слоны самые слонистые слоны в мире!" говорит о том, что советские МФ лучшие в мире!');
+var result = parser.parseString('Компания <strong>"ООО "<u>Рога и копыта</u>"</strong> производит рога и копыта... Цитата: <span style=\"colcor: red;\">"</span>Покупайте наших слонов! Наши слоны самые слонистые слоны в мире!" говорит о том, что советские МФ лучшие в мире!');
 
 console.log("Source:\n");
 console.log(result.getSourceString());
