d3a791e0f5d433af2f0746ac224a1d18d5fc1c34
Pavel Ladygin <me@digitorum.ru>
2018-01-11 10:54:32 +0300

    Постобработка "инициалов" вынесена из парсера в бусты.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulpConcat = require('gulp-concat');
 var gulpUglifyJS = require("gulp-uglifyjs");
 
 gulp.task('default', function () {
-    var orderedFiles = ['lib/parserStates.js', 'lib/produce.js', 'lib/parser.js'];
+    var orderedFiles = ['lib/parserStates.js', 'lib/produce.js', 'lib/boost/*.js', 'lib/parser.js'];
 
     gulp.src(orderedFiles).pipe(gulpConcat('tok3n1zer.js')).pipe(gulp.dest('dist/'));
     gulp.src(orderedFiles).pipe(gulpConcat('tok3n1zer.min.js')).pipe(gulpUglifyJS()).pipe(gulp.dest('dist/'));
